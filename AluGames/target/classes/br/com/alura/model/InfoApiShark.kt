bce7ebd8e7200186fd67c01a22ae8c7e12ac2eae
Matheus Inácio <matheus.inacio@outlook.com>
2024-10-08 17:14:11 -0300

    Delete AluGames/target/classes/br/com/alura directory

--- AluGames/target/classes/br/com/alura/model/InfoApiShark.kt
+++
@@ -1,3 +0,0 @@
-package br.com.alura.model
-
-data class InfoApiShark(val title: String, val thumb: String)
--- AluGames/target/classes/br/com/alura/model/InfoJogo.kt
+++
@@ -1,7 +0,0 @@
-package br.com.alura.model
-
-data class InfoJogo(val info: InfoApiShark) {
-    override fun toString(): String {
-        return info.toString()
-    }
-}
